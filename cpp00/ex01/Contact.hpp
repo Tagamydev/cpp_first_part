a002da4527c35dceaa18c416523c9b72c622e000
smsanchez <smsanchez@typsa.es>
2025-04-21 12:04:00 +0200

    fix(includes and headers)

--- cpp00/ex01/Contact.hpp
+++ cpp00/ex01/Contact.hpp
@@ -1,24 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   classi.hpp                                         :+:      :+:    :+:   */
+/*   class.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: samusanc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/11 08:43:06 by samusanc          #+#    #+#             */
-/*   Updated: 2023/12/11 11:37:34 by samusanc         ###   ########.fr       */
+/*   Created: 2023/12/11 08:37:24 by samusanc          #+#    #+#             */
+/*   Updated: 2023/12/11 11:45:26 by samusanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <ClassContact.hpp>
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+# include <iostream>
+# include <string>
+# define LEN 857
 
-Contact::Contact() {
-  this->visible = 0;
-  std::cout << "Constructor Contact called" << std::endl;
-  return;
-}
+class  Contact {
+ public:
+    char  first_name[857];
+    char  last_name[857];
+    char  nickname[857];
+    char  phone_number[857];
+    char  darkest_secret[857];
+    int    visible;
 
-Contact::~Contact() {
-  std::cout << "Destructor Contact called" << std::endl;
-  return;
-}
+    Contact();
+    ~Contact();
+};
+
+#endif
+
