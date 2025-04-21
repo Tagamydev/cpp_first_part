258fefa6c99c43a3e7986e3e6176e260aa72b3ad
smsanchez <smsanchez@typsa.es>
2025-04-21 11:54:06 +0200

    fix(cpp 00)

---
+++ cpp00/ex01/ClassPhoneBook.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   class.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: samusanc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/11 08:37:24 by samusanc          #+#    #+#             */
+/*   Updated: 2023/12/11 11:45:16 by samusanc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef  CLASSPHONEBOOK_HPP
+# define CLASSPHONEBOOK_HPP
+
+# include <iostream>
+# include <Contact.hpp>
+# include <limits>
+# include <cstdlib>
+
+
+class  PhoneBook {
+ public:
+    void  addcontact(void);
+    void  search(void);
+    PhoneBook();
+    ~PhoneBook();
+ private:
+    Contact  contacts[8];
+    int    ii;
+    void  seecontact(size_t y);
+};
+
+#endif
+
